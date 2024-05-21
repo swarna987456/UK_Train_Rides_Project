d100b40dc1e9265e3d88d9eb51a193b9f4bbf14d
swarna_ch <swarna.choppella2022@gmail.com>
2024-05-21 16:13:43 +0530

    Modified

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -6,12 +6,6 @@ from streamlit_option_menu import option_menu
 
 outputs = pickle.load(open('outputs.pkl', 'rb'))
 
-#Menu creation
-selected = option_menu(None, ["Project Proposal", "Popular Routes", "Peak Travel Times", "Revenue by Ticket Class & Type","On-time performance"], 
-    icons=['person-workspace', 'train-front', 'clock', 'bar-chart-line', 'hourglass-split'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-
-
 # Defining the custom template
 custom_template = {
     'layout': {
@@ -42,12 +36,15 @@ fig3 = px.bar(data_frame = outputs['Peak Travel Hours'].sort_index().reset_index
             orientation='h',color = 'Travel_Time_Segments', template='plotly', color_discrete_sequence=color_list)
 fig3.update_layout(yaxis={'dtick': 1})
 
-col1, col2 = st.columns(2)
+col1, col2,col3 = st.columns(3)
 with col1:
-    st.plotly_chart(fig1, use_container_width=False)
+    st.plotly_chart(fig1, use_container_width=True)
 
 with col2:
-    st.plotly_chart(fig2, use_container_width=False)
+    st.plotly_chart(fig2, use_container_width=True)
+
+with col3:
+    st.plotly_chart(fig3, use_container_width=True)
 
 
 
